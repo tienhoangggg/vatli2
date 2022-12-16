1b2b886e9142386ec2520aa6dd9a999c8dfbe832
tienhoangg <hoangtrantien202@gmail.com>
2022-12-16 15:33:46 +0700

    wifi,mqtt

---
+++ main.ino
@@ -0,0 +1,68 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+int trig_pin = 2;
+int echo_pin = 4;
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* idDevice = "30t41975"
+
+//***Set server***
+const char* mqttServer = "broker.mqtt-dashboard.com"; 
+int port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void wifiConnect() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void mqttReconnect() {
+  while(!client.connected()) {
+    if(client.connect(idDevice))) {
+
+      //***Subscribe all topic you need***
+      client.subscribe("binhluuluong/");
+    }
+    else {
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  wifiConnect();
+  client.setServer(mqttServer, port);
+  client.setCallback(callback);
+}
+
+//MQTT Receiver
+void callback(char* topic, byte* message, unsigned int length) {
+  String strMsg;
+  for(int i=0; i<length; i++) {
+    strMsg += (char)message[i];
+  }
+}
+
+long getDistance(){
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  long duration = pulseIn(echo_pin, HIGH);
+  long distanceCm = duration * 0.034 / 2;
+  return distanceCm;
+}
+
+void loop() {
+  if(!client.connected()) {
+    mqttReconnect();
+  }
+  client.loop();
+}
