9d9fe9eb23fe8a6a9a99d10521699772ec5617d9
tienhoangg <hoangtrantien202@gmail.com>
2022-12-20 18:20:45 +0700

    .

--- main.ino
+++ main.ino
@@ -2,6 +2,8 @@
 #include <PubSubClient.h>
 int trig_pin = 2;
 int echo_pin = 4;
+int addDevice_pin = 0;
+int reset_pin = 0;
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 const char* idDevice = "30t41975"
@@ -58,6 +60,25 @@ long getDistance(){
   long duration = pulseIn(echo_pin, HIGH);
   long distanceCm = duration * 0.034 / 2;
   return distanceCm;
+}
+
+long volume(){
+  long h = 50 - getDistance();
+  long v = 0;
+  if (h > 20) {
+    v += (h-20)*1256;
+    h = 20;
+  }
+  v += 25120 * h * h * h / 24000;
+  return v;
+}
+
+void throwOut(long v){
+  long v_after = volume() - v;
+  if (v_after < 0) v_after = 0;
+  turnServo(true);
+  while(volume() > v_after);
+  turnServo(false);
 }
 
 void loop() {
